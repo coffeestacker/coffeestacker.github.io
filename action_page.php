06179c007a73523202decc0154542eb71015e992
Melissa Du <mdu96@stanford.edu>
2019-06-12 02:02:39 +0000

    form experimenting

--- action_page.php
+++ action_page.php
@@ -1,8 +1,8 @@
 <?php
   // The global $_POST variable allows you to access the data sent with the POST method by name
   // To access the data sent with the GET method, you can use $_GET
-  $wechatID = htmlspecialchars($_POST['wechatID']);
+  $wechatID = htmlspecialchars($_POST['wechatID2']);
   // $to  = htmlspecialchars($_POST['to']);
 
-  echo  $wechatID;
+  echo $wechatID;
 ?>
