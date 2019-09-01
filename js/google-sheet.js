250c1843bd74395350b8208f4a5304b6065324e6
Melissa Du <mdu96@stanford.edu>
2019-09-01 15:49:05 -0700

    test

--- js/google-sheet.js
+++ js/google-sheet.js
@@ -1,14 +1,77 @@
-var $form = $('form#submit-to-google-sheet'),
-    url = 'https://script.google.com/macros/s/AKfycbw-FEKEjZ8dC-XCyCEKOI09HHBigdsN5bSr37JZDHB5eM6L6Hk/exec'
+// Variable to hold request
+var request;
 
-$('#submit-form').on('click', function(e) {
-  e.preventDefault();
-  var jqxhr = $.ajax({
-    url: url,
-    method: "GET",
-    dataType: "json",
-    data: $form.serializeObject()
-  }).success(
-    // do something
-  );
-})
+// Bind to the submit event of our form
+$("#submit-form").click(function(event){
+    console.log("HERE");
+    // Abort any pending request
+    if (request) {
+        request.abort();
+    }
+    // setup some local variables
+    var $form = $(this);
+
+    // Let's select and cache all the fields
+    var $inputs = $form.find("input, select, button, textarea");
+
+    // Serialize the data in the form
+    var serializedData = $form.serialize();
+
+    // Let's disable the inputs for the duration of the Ajax request.
+    // Note: we disable elements AFTER the form data has been serialized.
+    // Disabled form elements will not be serialized.
+    $inputs.prop("disabled", true);
+
+    // Fire off the request to /form.php
+    request = $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbw-FEKEjZ8dC-XCyCEKOI09HHBigdsN5bSr37JZDHB5eM6L6Hk/exec",
+        type: "post",
+        data: serializedData
+    });
+
+    // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        $form[0].reset(); // Clear the form
+        // Log a message to the console
+        console.log("Hooray, it worked!");
+        console.log(response);
+        console.log(textStatus);
+        console.log(jqXHR);
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // Log the error to the console
+        console.error(
+            "The following error occurred: "+
+            textStatus, errorThrown
+        );
+    });
+
+    // Callback handler that will be called regardless
+    // if the request failed or succeeded
+    request.always(function () {
+        // Reenable the inputs
+        $inputs.prop("disabled", false);
+    });
+
+    // Prevent default posting of form
+    event.preventDefault();
+});
+
+
+
+// var $form = $('form#submit-to-google-sheet'),
+//     url = 'https://script.google.com/macros/s/AKfycbw-FEKEjZ8dC-XCyCEKOI09HHBigdsN5bSr37JZDHB5eM6L6Hk/exec'
+
+// $('#submit-form').on('click', function(e) {
+//   e.preventDefault();
+//   var jqxhr = $.ajax({
+//     url: url,
+//     method: "GET",
+//     dataType: "json",
+//     data: $form.serializeObject()
+//   }).success(
+//     console.log("form success")
+//   );
+// })
