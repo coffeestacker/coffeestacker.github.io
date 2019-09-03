61c2d4d7cb213720f0fe95da040b348c39bc3e5a
Melissa Du <mdu96@stanford.edu>
2019-09-02 19:40:48 -0700

    new

--- js/google-sheet.js
+++ js/google-sheet.js
@@ -1,9 +1,76 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwGLEQFoKH3q9FuElNhNWeV9SjomlOTF7KXDeNCT2IImuhgD1g/exec'
-const form = document.forms['submit-to-google-sheet']
-form.addEventListener('submit', e => {
-  console.log("here");
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message))
-})
+// Variable to hold request
+var request;
+console.log("HERE");
+// Bind to the submit event of our form
+$("#submit-to-google-sheet").submit(function(event){
+    console.log("made it")
+    // Abort any pending request
+    if (request) {
+        request.abort();
+    }
+    // setup some local variables
+    var $form = $(this);
+
+    // Let's select and cache all the fields
+    var $inputs = $form.find("input, select, button, textarea");
+
+    // Serialize the data in the form
+    var serializedData = $form.serialize();
+
+    // Let's disable the inputs for the duration of the Ajax request.
+    // Note: we disable elements AFTER the form data has been serialized.
+    // Disabled form elements will not be serialized.
+    $inputs.prop("disabled", true);
+
+    // Fire off the request to /form.php
+    request = $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbwGLEQFoKH3q9FuElNhNWeV9SjomlOTF7KXDeNCT2IImuhgD1g/exec",
+        type: "post",
+        data: serializedData
+    });
+
+    // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        $form[0].reset(); // Clear the form
+        // Log a message to the console
+        console.log("Hooray, it worked!");
+        console.log(response);
+        console.log(textStatus);
+        console.log(jqXHR);
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // Log the error to the console
+        console.error(
+            "The following error occurred: "+
+            textStatus, errorThrown
+        );
+    });
+
+    // Callback handler that will be called regardless
+    // if the request failed or succeeded
+    request.always(function () {
+        // Reenable the inputs
+        $inputs.prop("disabled", false);
+    });
+
+    // Prevent default posting of form
+    event.preventDefault();
+});
+
+
+
+
+
+
+
+
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbwGLEQFoKH3q9FuElNhNWeV9SjomlOTF7KXDeNCT2IImuhgD1g/exec'
+// const form = document.forms['submit-to-google-sheet']
+// form.addEventListener('submit', e => {
+//   e.preventDefault()
+//   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//     .then(response => console.log('Success!', response))
+//     .catch(error => console.error('Error!', error.message))
+// })
